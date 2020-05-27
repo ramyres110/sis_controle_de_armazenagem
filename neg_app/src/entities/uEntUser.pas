fe0e7ce3f29755ac870fed66a25121737cef58e7
ramyres110 <ramyres90@gmail.com>
2020-05-26 22:45:24 -0300

    Alterando usuário de controle para entidade usuário. foi alterado de Integer para TUser

--- neg_app/src/entities/uEntUser.pas
+++ neg_app/src/entities/uEntUser.pas
@@ -8,23 +8,27 @@ type
     FId: Integer;
     FUsername: String;
     FPassword: String;
-    FCreatedBy: Integer;
+    FCreatedBy: TUser;
     FCreatedAt: TDate;
-    FChangedBy: Integer;
+    FChangedBy: TUser;
     FChangedAt: TDate;
   public
     constructor Create;
-    destructor Destrou;
+    destructor Destroy; override;
+
     property id: Integer read FId write FId;
     property username: String read FUsername write FUsername;
     property password: String read FPassword write FPassword;
-    property createdBy: Integer read FCreatedBy write FCreatedBy;
+    property createdBy: TUser read FCreatedBy write FCreatedBy;
     property createdAt: TDate read FCreatedAt write FCreatedAt;
-    property changedBy: Integer read FChangedBy write FChangedBy;
+    property changedBy: TUser read FChangedBy write FChangedBy;
     property changedAt: TDate read FChangedAt write FChangedAt;
   end;
 
 implementation
+
+uses
+  System.SysUtils;
 
 { TUser }
 
@@ -33,9 +37,17 @@ begin
 
 end;
 
-destructor TUser.Destrou;
+destructor TUser.Destroy;
 begin
-
+  if (Self.FCreatedBy <> nil) then
+  begin
+    FreeAndNil(Self.FCreatedBy);
+  end;
+  if (Self.FChangedBy <> nil) then
+  begin
+    FreeAndNil(Self.FChangedBy);
+  end;
+  inherited;
 end;
 
 end.
